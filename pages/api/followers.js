93dd43f325c8f10bf1358c76c550599779c1d47d
smog <irakli1393@gmail.com>
2022-04-09 22:16:02 +0400

    make two api endppoints - project and follwoers to fetch data

---
+++ pages/api/followers.js
@@ -0,0 +1,124 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+import "isomorphic-unfetch";
+const PROJECT_OWNER_PROFILE_ID = "0xf3";
+import { createClient, gql } from "@urql/core";
+const APIURL = "https://api-mumbai.lens.dev/";
+
+const getFollowers = async (profileId) => {
+  const client = new createClient({
+    url: APIURL,
+  });
+  const QUERY = gql`
+    query ($request: FollowersRequest!) {
+      followers(request: $request) {
+        items {
+          wallet {
+            address
+            defaultProfile {
+              id
+              name
+              bio
+              location
+              website
+              twitterUrl
+              handle
+              picture {
+                ... on NftImage {
+                  contractAddress
+                  tokenId
+                  uri
+                  verified
+                }
+                ... on MediaSet {
+                  original {
+                    url
+                    mimeType
+                  }
+                }
+              }
+              coverPicture {
+                ... on NftImage {
+                  contractAddress
+                  tokenId
+                  uri
+                  verified
+                }
+                ... on MediaSet {
+                  original {
+                    url
+                    mimeType
+                  }
+                }
+              }
+              ownedBy
+              depatcher {
+                address
+                canUseRelay
+              }
+              stats {
+                totalFollowers
+                totalFollowing
+                totalPosts
+                totalComments
+                totalMirrors
+                totalPublications
+                totalCollects
+              }
+              followModule {
+                ... on FeeFollowModuleSettings {
+                  type
+                  contractAddress
+                  amount {
+                    asset {
+                      name
+                      symbol
+                      decimals
+                      address
+                    }
+                    value
+                  }
+                  recipient
+                }
+              }
+            }
+          }
+          totalAmountOfTimesFollowed
+        }
+        pageInfo {
+          prev
+          next
+          totalCount
+        }
+      }
+    }
+  `;
+  let response = await client
+    .query(QUERY, {
+      request: {
+        profileId,
+        limit: 40,
+      },
+    })
+    .toPromise();
+  return response;
+};
+
+export default async function handler(req, res) {
+  if (req.method === "GET") {
+    try {
+      //get followers of project owner
+      const followersData = await getFollowers(PROJECT_OWNER_PROFILE_ID);
+
+      //retrun project json
+      res.status(200).json({ data: { followers: followersData.data } });
+    } catch (error) {
+      res
+        .status(500)
+        .json({ error: "API Error", internalMessage: error.message });
+    }
+  } else {
+    res.setHeader("Allow", "POST");
+    res.status(405).end("Method Not Allowed");
+  }
+}
--- pages/api/project.js
+++ pages/api/project.js
@@ -1,27 +1,54 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
+const fetch = (...args) =>
+  import("node-fetch").then(({ default: fetch }) => fetch(...args));
+import { ethers, Wallet, utils, BigNumber } from "ethers";
+import { LENS_HUB_ABI } from "@src/shared/ABIs";
+
+const LENS_HUB_CONTRACT = "0x4BF0c7AD32Fd2d32089790a54485e23f5C7736C0";
+const PROJECT_OWNER_ADDRESS_PRIVATE_KEY =
+  "a544578cc0e6977f9d15e1b71e74dbc58edd512e12bef701b6cc49d3351582c7";
 const PROJECT_OWNER_PROFILE_ID = "0xf3";
 const PROJECT_LENS_PUBLICATION_ID = "0x0b";
+const ethersProvider = new ethers.providers.JsonRpcProvider(
+  "https://rpc-mumbai.maticvigil.com"
+);
 
-//call contract
-//fetch ipfs link
-//send data back
+const getSigner = () => {
+  return new Wallet(PROJECT_OWNER_ADDRESS_PRIVATE_KEY, ethersProvider);
+};
+
+const getPostContentURIFromContract = async (profileId, postId) => {
+  const lensHub = new ethers.Contract(
+    LENS_HUB_CONTRACT,
+    LENS_HUB_ABI,
+    getSigner()
+  );
+  const profile = BigNumber.from(profileId).toNumber();
+  const post = BigNumber.from(postId).toNumber();
+  const data = await lensHub.getPub(profile, post);
+
+  return data.contentURI;
+};
 
 export default async function handler(req, res) {
   if (req.method === "GET") {
     try {
-      const existingUsername = await repository.findOneDoc(
-        COLLECTIONS.USERNAMES,
-        { username: req.body.username }
+      //get contetURI from lens contract by profileId and pubId
+      const url = await getPostContentURIFromContract(
+        PROJECT_OWNER_PROFILE_ID,
+        PROJECT_LENS_PUBLICATION_ID
       );
-      if (existingUsername) {
-        throw new Error("such username already exists please take another one");
-      }
-      const savedUsername = await repository.saveDoc(
-        COLLECTIONS.USERNAMES,
-        req.body
-      );
-      res.status(200).json({ data: { userName: savedUsername } });
+      //fetch IPFS json file
+      const ipfsResponse = await fetch(url, {
+        headers: {
+          accept: "application/json",
+        },
+      });
+      const ipfsData = await ipfsResponse.json();
+
+      //retrun project json
+      res.status(200).json({ data: { project: ipfsData } });
     } catch (error) {
       res
         .status(500)
